2535d0bfbcc4ad600ece4854581e0686c86e1187
dentopiasa <castelli_philippe@hotmail.com>
2024-01-26 19:33:24 +0200

    cat

--- test_platform/src/App.jsx
+++ test_platform/src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import Contact from './Components/Contact';
 import Display from './Components/Display';
 import React from 'react';
+import { useEffect } from 'react';
 
 function App() { //state function here defines display value
   const [name , setName] = useState("")
   const [email , setEmail] = useState("")
   const [password , setPassword] = useState("")
+
+  const [counter , setCounter] = useState(0)
 
 
   const setNameChange = (value) => {
@@ -20,10 +23,32 @@ function App() { //state function here defines display value
     setPassword(value)
   }
 
+  /*useEffect(()=> {
+    const key = setInterval(()=> {
+      setCounter(count => count +1)
+    }, 1000);
+    return () => {
+      clearInterval(key)
+    };
+  }, []*/
+
+  function jason() {
+    fetch('https://pokeapi.co/api/v2/pokemon/', {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response);
+      return (
+        <p></p>
+      )
+    }); 
+  }
 
   return (
     <div className='mainContainer'>
-    <h1>hello user</h1>
+    <h1>hello user </h1>
+    {jason()}
     <h2>this is a test page</h2>
     <h3>please enter your credentials:</h3>
     <Contact
@@ -38,6 +63,7 @@ function App() { //state function here defines display value
         password={password}
         />
       </aside>
+      <p>{counter}</p>
     </div>
   )
 }
